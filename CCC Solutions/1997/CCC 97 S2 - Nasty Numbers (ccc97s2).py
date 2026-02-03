6cdb41c9af140b90013e07c13bbf534d004e1733
dchoo333 <154945135+dchoo333@users.noreply.github.com>
2026-02-03 17:45:07 +0800

    CCC 1997 Solutions

--- CCC Solutions/1997/CCC 97 S2 - Nasty Numbers (ccc97s2).py
+++ CCC Solutions/1997/CCC 97 S2 - Nasty Numbers (ccc97s2).py
@@ -1,2 +1,18 @@
-for num in [int(input()) for _ in range(int(input()))]:
-    print(f"{num} is nasty") if num % 6 == 0 else print(f"{num} is not nasty")
+for _ in range(int(input())):
+    n = int(input())
+    p = []
+    m = int(n ** 0.5)
+    for i in range(1, m + 1):
+        q, r = divmod(n, i)
+        if r == 0:
+            p.append(q - i)
+            p.append(i + q)
+    yes = False
+    for x in p:
+        if p.count(x) != 1:
+            yes = True
+            break
+    if yes:
+        print(f"{n} is nasty")
+    else:
+        print(f"{n} is not nasty")
