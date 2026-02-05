ad209a531c59b875950082e2c6d25c2c37a48e15
dchoo333 <154945135+dchoo333@users.noreply.github.com>
2026-02-05 09:25:32 +0800

    Delete CCC Solutions/2001 directory

--- CCC Solutions/2001/CCC 01 J2 - Mod Inverse (ccc01j2).py
+++
@@ -1,9 +0,0 @@
-a, b, n = int(input()), int(input()), True
-
-for x in range(1000):
-    if (a * x - 1) % b == 0:
-        print(x)
-        n = False
-        break
-
-print("No such integer exists.") if n else ''
