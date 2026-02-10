7cf41151b7b4893317c23fc81408b94b4009601b
dchoo333 <154945135+dchoo333@users.noreply.github.com>
2026-02-10 19:28:47 +0800

    Delete CCC Solutions/2009 directory

--- CCC Solutions/2009/CCC 09 J2 - Old Fishin' Hole (ccc09j2).py
+++
@@ -1,9 +0,0 @@
-trout, pike, pickerel, total, count = int(input()), int(input()), int(input()), int(input()), 0
-for i in range(total+1):
-    for j in range(total+1):
-        for k in range(total+1):
-            if (i*trout) + (j*pike) + (k*pickerel) <= total and i+j+k > 0:
-                print(f"{i} Brown Trout, {j} Northern Pike, {k} Yellow Pickerel")
-                count += 1
-print(f"Number of ways to catch fish: {count}")
-
